5c8db58e3809954258e8c314d3aa5e0a5f4e7591
jongco0331 <jongco0331@gmail.com>
2026-04-09 01:26:36 +0900

    refactor: jwt 환경 설정 유틸
    - 환경 변수 하드 코딩에서 yml에서 주입받는 방식으로 변경.

--- backend/src/main/java/kr/jonghyun/noticeboard/NoticeboardApplication.java
+++ backend/src/main/java/kr/jonghyun/noticeboard/NoticeboardApplication.java
@@ -1,11 +1,14 @@
 package kr.jonghyun.noticeboard;
 
+import kr.jonghyun.noticeboard.global.config.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication
+@EnableConfigurationProperties(JwtProperties.class)
 public class NoticeboardApplication {
 
     public static void main(String[] args) {
--- backend/src/main/java/kr/jonghyun/noticeboard/global/config/JwtUtil.java
+++ backend/src/main/java/kr/jonghyun/noticeboard/global/config/JwtUtil.java
@@ -2,25 +2,30 @@ package kr.jonghyun.noticeboard.global.config;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 
 @Component
+@RequiredArgsConstructor
 public class JwtUtil {
 
-    private final SecretKey key = Keys.hmacShaKeyFor(
-            "noticeboard-secret-key-must-be-32bytes!!".getBytes()
-    );
+    private final JwtProperties jwtProperties;
+    private SecretKey key;
 
-    private final long EXPIRATION = 1000 * 60 * 60 * 1; // 1시간
+    @PostConstruct
+    public void init() {
+        key = Keys.hmacShaKeyFor(jwtProperties.secret().getBytes());
+    }
 
     public String generateToken(String identifier) {
         return Jwts.builder()
                 .subject(identifier)
                 .issuedAt(new Date())
-                .expiration(new Date(System.currentTimeMillis() + EXPIRATION))
+                .expiration(new Date(System.currentTimeMillis() + jwtProperties.expiration()))
                 .signWith(key)
                 .compact();
     }
